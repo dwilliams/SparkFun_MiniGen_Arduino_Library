4d5317b4cbb6d7e10ba771142c046b6feaa6a57d
Daniel Williams <dwilliams@inovonics.com>
2016-07-21 14:29:04 -0600

    Updated to the new SPI API.

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -19,18 +19,19 @@ Code developed in Arduino 1.0.5, on an Arduino Pro Mini 5V.
 #include "SparkFun_MiniGen.h"
 #include <SPI.h>
 
+//// NOT USING THIS ANYMORE ////
 // configSPIPeripheral() is an abstraction of the SPI setup code. This
 //  minimizes difficulty in porting to a new target: simply change this
 //  code to fit the new target.
-void MiniGen::configSPIPeripheral()
-{
-  SPI.setDataMode(SPI_MODE2);  // Clock idle high, data capture on falling edge
-  pinMode(_FSYNCPin, OUTPUT);  // Make the FSYCPin an output; this is analogous
-                               //  to chip select in most systems.
-  pinMode(10, OUTPUT);
-  digitalWrite(_FSYNCPin, HIGH);
-  SPI.begin();
-}
+// void MiniGen::configSPIPeripheral()
+// {
+  // SPI.setDataMode(SPI_MODE2);  // Clock idle high, data capture on falling edge
+  // pinMode(_FSYNCPin, OUTPUT);  // Make the FSYCPin an output; this is analogous
+                               // //  to chip select in most systems.
+  // pinMode(10, OUTPUT);
+  // digitalWrite(_FSYNCPin, HIGH);
+  // SPI.begin();
+// }
 
 // SPIWrite is optimized for this part. All writes are 16-bits; some registers
 //  require multiple writes to update all bits in the registers. The address of
@@ -38,9 +39,20 @@ void MiniGen::configSPIPeripheral()
 //  functions will properly prepare the data with that information.
 void MiniGen::SPIWrite(uint16_t data)
 {
-  digitalWrite(_FSYNCPin, LOW);
-  SPI.transfer((byte)(data>>8));
-  SPI.transfer((byte)data);
-  digitalWrite(_FSYNCPin, HIGH);
+  // Converting this to use the new SPI interface bits on a transactional basis.  The transactional basis will allow
+  //   the SPI bus to be used with different devices and libraries.
+  
+  // BeginTransaction on the SPI bus.  AD9837 is rated to 40MHz bus clock, but limiting to 10MHz as that should be fast
+  //   enough.  AD9837 also uses SPI Mode 2 (CPOL = 1, CPHA = 0).
+  SPI.beginTransaction( SPISettings( 10000000, MSBFIRST, SPI_MODE2));
+  digitalWrite( _FSYNCPin, LOW);
+  
+  // Write the data.
+  SPI.transfer((byte) (data>>8));
+  SPI.transfer((byte) data);
+  
+  // EndTransaction to release bus for other chips to use.
+  SPI.EndTransaction();
+  digitalWrite( _FSYNCPin, HIGH);
 }
 
